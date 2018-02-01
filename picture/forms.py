c54e28e253ec2be154504fea73eb9bfda1200d72
HCY <1241908493@qq.com>
2018-02-01 16:36:43 +0800

    edit .gitignore

--- picture/forms.py
+++
@@ -1,8 +0,0 @@
-#encoding:utf-8
-from django import forms
-from .models import Picture
-
-class PictureForm(forms.ModelForm):
-    class Meta:
-        model=Picture
-        fields = '__all__'
--- picture/models.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
-class Picture(models.Model):
-    image=models.ImageField(upload_to='images/%Y/%m',max_length=500)
-    created=models.DateTimeField(auto_now_add=True)
